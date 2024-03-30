3aed38717cb1d37d9600cd8c6e249bcc0216f351
OLADIMEJI GIDEON EMILOLORUN <150672702+oladimeji221@users.noreply.github.com>
2024-03-30 18:11:56 +0100

    contact.php

--- contact.php
+++ contact.php
@@ -1,20 +1,28 @@
 <?php
+session_start();
 
-
-if (isset($_POST['submit'] )) {
+if (isset($_POST['submit'])) {
     $firstname = $_POST['firstname'];
-    $lastname  = $_POST['lastname'];
-    $whatsapp  = $_POST['whatsapp'];
-    $message  = $_POST['message'];
+    $lastname = $_POST['lastname'];
+    $whatsapp = $_POST['whatsapp'];
+    $message = $_POST['message'];
 
+    $mailTo = "kolade@kdxchange.com.ng, koladeo49@gmail.com";
+    $headers = "From: " . $mailFrom;
+    $txt = "You have a message from " . $firstname . " " . $lastname . " with WhatsApp: " . $whatsapp . ".\n\n" . $message;
 
-    $mailTo = "koladeo49@gmail.com";
-    $headers = "from: ".$mailFrom;
-    $txt = "You have a message ".$name.".\n\n".$message;
-
-
-    mail($mailTo, $name, $txt, $headers);
-
-    header("location: index.html?MessageSent");
+    if (mail($mailTo, 'Message from ' . $firstname . ' ' . $lastname, $txt, $headers)) {
+        $_SESSION['message'] = "Message sent successfully!";
+    } else {
+        $_SESSION['message'] = "Message sending failed. Please try again.";
+    }
+    
+   // if (isset($_SESSION['message'])) {
+   //      echo '<script>alert("' . $_SESSION['message'] . '");</script>';
+   //      unset($_SESSION['message']); // Clear the message after displaying
+   //      echo '<script>window.location.href = "index.html";</script>'; // Redirect after showing the alert
+   //      exit(); // Exit the script
+   //  }
+    
 }
 ?>
