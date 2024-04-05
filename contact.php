9c3641d6974e3b90d81df6c68f69622b72b6a6d5
OLADIMEJI GIDEON EMILOLORUN <150672702+oladimeji221@users.noreply.github.com>
2024-04-05 13:42:31 +0100

    contact.php

--- contact.php
+++ contact.php
@@ -12,17 +12,17 @@ if (isset($_POST['submit'])) {
     $txt = "You have a message from " . $firstname . " " . $lastname . " with WhatsApp: " . $whatsapp . ".\n\n" . $message;
 
     if (mail($mailTo, 'Message from ' . $firstname . ' ' . $lastname, $txt, $headers)) {
-        $_SESSION['$message'] = "Message sent successfully!";
+        $_SESSION['message'] = "Message sent successfully!";
     } else {
-        $_SESSION['$message'] = "Message sending failed. Please try again.";
+        $_SESSION['message'] = "Message sending failed. Please try again.";
     }
     
-   // if (isset($_SESSION['message'])) {
-   //      echo '<script>alert("' . $_SESSION['message'] . '");</script>';
-   //      unset($_SESSION['message']); // Clear the message after displaying
-   //      echo '<script>window.location.href = "index.html";</script>'; // Redirect after showing the alert
-   //      exit(); // Exit the script
-   //  }
+   if (isset($_SESSION['message'])) {
+        echo '<script>alert("' . $_SESSION['message'] . '");</script>';
+        unset($_SESSION['message']); // Clear the message after displaying
+        echo '<script>window.location.href = "index.html";</script>'; // Redirect after showing the alert
+        exit(); // Exit the script
+    }
     
 }
 ?>
